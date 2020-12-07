ab2a89546cd1a1a97b18ef608e23a202bed04ec0
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-12-07 13:11:57 +0100

    Añade constructores en clase inventada

--- src/main/java/com/mycompany/tarea4isaacjimenez/Ejemplo.java
+++ src/main/java/com/mycompany/tarea4isaacjimenez/Ejemplo.java
@@ -14,4 +14,15 @@ public class Ejemplo {
     int atributo1;
     int atributo2;
     int atributo3;
+
+    public Ejemplo() {
+    }
+
+    public Ejemplo(int atributo1, int atributo2, int atributo3) {
+        this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
+        this.atributo3 = atributo3;
+    }
+    
+    
 }
