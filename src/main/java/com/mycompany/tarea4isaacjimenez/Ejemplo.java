48899a5b852b825c150290c722da146b72f879ae
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-12-07 12:55:58 +0100

    Añade atributos en clase modelo

---
+++ src/main/java/com/mycompany/tarea4isaacjimenez/Ejemplo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tarea4isaacjimenez;
+
+/**
+ *
+ * @author isaac
+ */
+public class Ejemplo {
+
+    int atributo1;
+    int atributo2;
+    int atributo3;
+}
