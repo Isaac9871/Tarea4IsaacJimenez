03b7077f13412a7f6f832ddb10dd8b39382695e8
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-12-07 13:16:53 +0100

    Añade getters/setters en clase inventada

--- src/main/java/com/mycompany/tarea4isaacjimenez/Ejemplo.java
+++ src/main/java/com/mycompany/tarea4isaacjimenez/Ejemplo.java
@@ -11,9 +11,9 @@ package com.mycompany.tarea4isaacjimenez;
  */
 public class Ejemplo {
 
-    int atributo1;
-    int atributo2;
-    int atributo3;
+    private int atributo1;
+    private int atributo2;
+    private int atributo3;
 
     public Ejemplo() {
     }
@@ -22,6 +22,30 @@ public class Ejemplo {
         this.atributo1 = atributo1;
         this.atributo2 = atributo2;
         this.atributo3 = atributo3;
+    }
+
+    public int getAtributo3() {
+        return atributo3;
+    }
+
+    public void setAtributo3(int atributo3) {
+        this.atributo3 = atributo3;
+    }
+
+    public int getAtributo1() {
+        return atributo1;
+    }
+
+    public void setAtributo1(int atributo1) {
+        this.atributo1 = atributo1;
+    }
+
+    public int getAtributo2() {
+        return atributo2;
+    }
+
+    public void setAtributo2(int atributo2) {
+        this.atributo2 = atributo2;
     }
     
     
